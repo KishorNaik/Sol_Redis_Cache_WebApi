13b8aaae6f8be407133317bbdd79060473d5267e
Kishor Naik <kishor.naik011.net@gmail.com>
2021-02-12 16:55:16 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Controllers
+{
+    [Route("api/redis")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly IDistributedCache distributedCache = null;
+
+        public DemoController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        [HttpGet("demo")]
+        public async Task<IActionResult> Demo()
+        {
+            try
+            {
+                var currentDateTimeObject = DateTime.Now.ToString();
+
+                var cacheDateTime = await this.distributedCache.GetStringAsync("CacheDateTime");
+                if (cacheDateTime == null)
+                {
+                    var distributeCacheEntryOption = new DistributedCacheEntryOptions()
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10)
+                    };
+                    await this.distributedCache.SetStringAsync("CacheDateTime", currentDateTimeObject, distributeCacheEntryOption);
+                    cacheDateTime = await this.distributedCache.GetStringAsync("CacheDateTime");
+                }
+
+                return base.Ok(currentDateTimeObject);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
